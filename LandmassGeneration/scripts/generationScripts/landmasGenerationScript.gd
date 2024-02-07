837f9f86482b81ec037398f6bdb9d9866555235b
Maiqtheterrorist <103211996+Maiqtheterrorist@users.noreply.github.com>
2024-02-07 21:05:30 +0200

    Add files via upload

--- LandmassGeneration/scripts/generationScripts/landmasGenerationScript.gd
+++ LandmassGeneration/scripts/generationScripts/landmasGenerationScript.gd
@@ -1,12 +1,12 @@
 extends Node
+
 class_name landmassGenerator
+
 static var fastNoiseLite : FastNoiseLite = FastNoiseLite.new()
-static var data : PackedByteArray
-@export var noiseWidth : int = 10
-@export var noiseHeight : int = 10
-@export var noiseScale : float = 1.0
-static var amplitude = 1.0
-static var frequency = 1.0
+
+static var octavesDictionary : Dictionary = {
+	
+}
 
 func _ready() -> void:
 	_fastNoiseLite_initializer()
@@ -15,12 +15,27 @@ func _fastNoiseLite_initializer():
 	fastNoiseLite.noise_type = FastNoiseLite.TYPE_PERLIN
 	fastNoiseLite.fractal_type = FastNoiseLite.FRACTAL_NONE
 
-static func _generate_noise_map(width, height, scale) -> ImageTexture:
+static func _generate_noise_map(width : int, height : int, scale : float, octaves : int, persistance : float, lacunarity : float) -> ImageTexture:
 	var noiseImage : Image = Image.create(width,height,false,Image.FORMAT_RGBA8)
-	for y in height:
-		for x in width:
-			var sampleY = y/scale * frequency
-			var sampleX = x/scale * frequency
-			noiseImage.set_pixel(x,y, Color.from_hsv(0.0, 0.0, (fastNoiseLite.get_noise_2d(sampleX,sampleY)+1)/2)*amplitude)
+	var usableFrequency = lacunarity
+	var usableAmplitude = persistance
+	
+	for i in octaves:
+		usableFrequency = pow(lacunarity, i)
+		usableAmplitude = pow(persistance, i)
+		prints(i,usableAmplitude,usableFrequency)
+		
+		var octave = []
+		
+		for y in height:
+			for x in width:
+				var sampleY = y/scale * usableFrequency
+				var sampleX = x/scale * usableFrequency
+				
+				var data = [Vector3(    sampleX, sampleY, ((fastNoiseLite.get_noise_2d(sampleX,sampleY)+1)/2)*usableAmplitude    )]
+				octave.append(data)
+				
+				noiseImage.set_pixel(x,y, Color.from_hsv(0.0, 0.0, (fastNoiseLite.get_noise_2d(sampleX,sampleY)+1)/2)*usableAmplitude)
+		octavesDictionary.merge({"Octave " + str(i) : octave})
 	var noiseTextureInstance : ImageTexture = ImageTexture.create_from_image(noiseImage)
 	return noiseTextureInstance
--- LandmassGeneration/scripts/utilityScripts/landmass2DNoisePreviewSetup.gd
+++ LandmassGeneration/scripts/utilityScripts/landmass2DNoisePreviewSetup.gd
@@ -1,6 +1,19 @@
 extends Node
-var delete = 100
+var total = 0
 
+var generationData : Dictionary = {
+	 "width" : 100,
+	"height" : 100,
+	"scale" : 0.5,
+	"octaves" : 3,
+	"persistance" : 0.5,
+	"lacunarity" : 2.0,
+	#"frequency" : 1.0,
+	#"amplitude" : 0.0
+}
+var finalValues : Dictionary = {
+	
+}
 @onready var noise_preview: MeshInstance3D = %noisePreview
 var material : StandardMaterial3D = StandardMaterial3D.new()
 var texture : ImageTexture
@@ -16,8 +29,18 @@ func _create_material_and_assign() -> void:
 	self.mesh.surface_set_material(0, material)
 
 func _create_noise_texture_and_assign() -> void:
-	material.albedo_texture = landmassGenerator._generate_noise_map(delete,delete, 27.3)
+	material.albedo_texture = landmassGenerator._generate_noise_map(generationData.get("width"), generationData.get("height"), generationData.get("scale"), generationData.get("octaves"), generationData.get("persistance"), generationData.get("lacunarity"))
 
-
-func _on_h_slider_value_changed(value: float) -> void:
-	material.albedo_texture = landmassGenerator._generate_noise_map(delete, delete, value)
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_up"):
+		#print(landmassGenerator.octavesDictionary)
+		var index = 0
+		for key in landmassGenerator.octavesDictionary:
+			#print("in " + str(key))
+			total += landmassGenerator.octavesDictionary.get(key)[0][0].z
+			print(landmassGenerator.octavesDictionary.get(key)[index][0].z)
+			index += 1
+			for value in landmassGenerator.octavesDictionary.get(str(key)):
+				print("at x: " + str(value[0].x) + " at y: " + str(value[0].y) + " value is :" + str(value[0].z))
+	#if Input.is_action_just_pressed("ui_left"):
+		#print(total)
